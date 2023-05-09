def83e67c279835acf6998bbc899716ea004cc1a
Krushna Patil <99466184+krushna530@users.noreply.github.com>
2023-05-09 21:53:25 +0530

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,1 +1,41 @@
+#include<iostream.h>
+#include<conio.h>
 
+void main()
+{
+  int a[2][2],b[2][2],c[2][2];
+  int i,j;
+  clrscr();
+  cout<<"Enter the first matrix :-"<<endl;
+  for(i=0;i<2;i++)
+  {
+   for(j=0;j<2;j++)
+   {
+     cin>>a[i][j];
+   }
+  }
+  cout<<"Enter the second matrix :-"<<endl;
+  for(i=0;i<2;i++)
+  {
+   for(j=0;j<2;j++)
+   {
+     cin>>b[i][j];
+   }
+  }
+  for(i=0;i<2;i++)
+  {
+   for(j=0;j<2;j++)
+   {
+     c[i][j]=a[i][j]+b[i][j];
+   }
+  }
+  cout<<"Addition of two matrix :-"<<endl;
+   for(i=0;i<2;i++)
+  {
+   for(j=0;j<2;j++)
+   {
+    cout<<c[i][j];
+   }
+   }
+   getch();
+}
